decd78d5386b74bfa086a64ad28ec40457ed5400
Iuri Diniz <iuridiniz@gmail.com>
2014-12-26 14:19:09 -0300

    correct bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
-
 # platforms
+if [ -d platforms ]; then
+    mv platforms platforms_
+    cordova platform add android
+    cordova platform add browser
+fi
 
-cordova platform add android
-cordova platform add browser
+if [ -d platforms_ ]; then
+    rsync -a platforms_/ platforms/
+    rm -rf platforms_
+fi
 
 # plugins 
+if [ -d plugins ]; then
+    mv plugins plugins_
+fi
+
 cordova plugin add org.apache.cordova.network-information
+
+if [ -d plugins_ ]; then
+    rsync -a plugins_/ plugins/
+    rm -rf plugins_
+fi
